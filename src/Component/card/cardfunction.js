b5c6f8371843ba87ea1ed97d5f6feebaf3563cb8
emon <emondas457@gmail.com>
2022-09-29 00:28:37 +0600

    addcard

--- src/Component/card/cardfunction.js
+++ src/Component/card/cardfunction.js
@@ -1,29 +1,30 @@
 
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import Addtocard from '../adddetail/addtocard';
 import Card from './card';
 const Cardfunction = () => {
-    const [gym,setgym] = useState([]);
-    useEffect(()=>{
-        fetch("products.json").then(res=>res.json()).then(data=>setgym(data));
+    const [gym, setgym] = useState([]);
+    useEffect(() => {
+        fetch("products.json").then(res => res.json()).then(data => setgym(data));
 
-    },[])
+    }, [])
     return (
         <div>
             <div className="grid grid-cols-4 gap-4">
-            <div className='col-span-3'>
-                <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12'>
-                {
-                     gym.map(gymvalue=>
-                        <Card gymdata={gymvalue}></Card> 
-                    )
-                }
-               
-              </div>
-              </div>
-            <div className='col-span-1 bg-[#2A303C] w-[90%] ml-[8%] rounded'>
+                <div className='col-span-3'>
+                    <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12'>
+                        {
+                            gym.map(gymvalue =>
+                                <Card gymdata={gymvalue}></Card>
+                            )
+                        }
 
-                
-            </div>
+                    </div>
+                </div>
+                <div className='col-span-1 bg-[#2A303C] w-[90%] ml-[8%] rounded'>
+                        <Addtocard></Addtocard>
+
+                </div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+  themes: [
+    {
+      mytheme: {
+
+        "primary": "#d1c9ff",
+
+        "secondary": "#f98b7a",
+
+        "accent": "#f7749b",
+
+        "neutral": "#312A3C",
+
+        "base-100": "#282C43",
+
+        "info": "#8DE5F7",
+
+        "success": "#11744C",
+
+        "warning": "#F1BD65",
+
+        "error": "#F22652",
+      },
+    },
+  ],
   plugins: [require("daisyui")],
 }
